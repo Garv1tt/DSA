1ed02e549fc4286c22f2013634fcbf55be5e93c0
Garvit <garvitt.242@gmail.com>
2026-08-02 18:22:02 +0530

    Patterns till 9 done

---
+++ patterns.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+using namespace std;
+// Pattern-1
+// int main(){
+//     for (int i=0;i<5;i++)
+//     {
+//         for(int j=0;j<5;j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+    
+//     return 0;
+// }
+// Pattern-2
+// int main(){
+//     for(int i=0;i<5;i++){
+//         for(int j=0;j<=i;j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// Pattern-3
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// Pattern-4
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=i;j++){
+//             cout << i;
+//         }
+//         cout << endl;
+//         return 0;
+//     }
+// }
+// Pattern-5
+// int main(){
+//     for(int i=0;i<=5;i++){
+//         for(int j=0;j<5-i;j++){
+//             cout << "*";
+//         }
+//         cout << endl;
+//       return 0;
+//     }
+// }
+// Pattern-6
+// int main(){
+//     for(int i=1;i<=5;i++){
+//         for(int j=1;j<=5-i+1;j++){
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// Pattern-7
+// int main(){
+//     for(int i=0;i<4;i++){
+//         for(int j=0;j<5-i-1;j++){
+//             cout << " ";
+//         }
+//         for(int k=0;k<2*i+1;k++){
+//             cout << "*";
+//         }
+//         for(int j=0;j<5-i-1;j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// Patternn-8
+// int main(){
+//     for(int i=0;i<=4;i++){
+//         for(int j=0;j<i;j++){
+//             cout << " ";
+//         }
+//         for(int k=0;k<2*5-2*i-1;k++){
+//             cout << "*";
+//         }
+//         for(int j=0;j<i;j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// Pattern-9
+// int main(){
+//     for(int i=0;i<5;i++){
+//         for(int j=0;j<5-i-1;j++){
+//             cout << " ";
+//         }
+//         for(int k=0;k<2*i+1;k++){
+//             cout << "*";
+//         }
+//         for(int j=0;j<5-i-1;j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+//     for(int i=0;i<=4;i++){
+//         for(int j=0;j<i;j++){
+//             cout << " ";
+//         }
+//         for(int k=0;k<2*5-2*i-1;k++){
+//             cout << "*";
+//         }
+//         for(int j=0;j<i;j++){
+//             cout << " ";
+//         }
+//         cout << endl;
+//     }
+// }
+// Pattern-10
+int main(){
+    
+    return 0;
+}
