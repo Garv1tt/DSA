4c2e7517e920becb2d6883c6dbb012cc9674a220
Garvit <garvitt.242@gmail.com>
2026-08-03 20:58:43 +0530

    done till Pattern-15

--- patterns.cpp
+++ patterns.cpp
@@ -150,3 +150,50 @@ using namespace std;
 //     }
 //     return 0;
 // }
+// Pattern-12
+// int main(){
+//     for(int i=1;i<6;i++){
+//         for(int j=1;j<=i;j++){
+//             cout << j;
+//         }
+//         for(int k=0;k<2*(5-i);k++){
+//             cout << " ";
+//         }
+//         for(int j=i;j>=1;j--){
+//             cout << j;
+//         }
+//         cout << endl;
+//     }
+// }
+// Pattern-13
+// int main(){
+//     int num=1;
+//     for(int i=0;i<4;i++){
+//         for(int j=0;j<=i;j++){
+//             cout << num << " ";
+//             num=num+1;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// Pattern-14
+// int main(){
+//     for(int i=0;i<6;i++){
+//         for(int j=0;j<=i;j++){
+//             cout << char('A'+j);
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+// Pattern-15
+// int main(){
+//     for(int i=0;i<5;i++){
+//         for(int j=0;j<5-i;j++){
+//             cout << char('A'+j);
+//         }   
+//         cout << endl; 
+//     }
+//     return 0;
+// }
